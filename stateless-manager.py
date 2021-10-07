69b6ddd10d7443b34742990dfef0b2b27c1fc4c5
berg <xiaocairush@163.com>
2021-10-07 18:28:48 +0800

    处理僵尸进程

--- stateless-manager.py
+++ stateless-manager.py
@@ -1,5 +1,6 @@
 import logging
 import time
+import signal
 
 from datetime import datetime, timedelta
 
@@ -42,6 +43,7 @@ logging.info(f"Found System Drives: {system_drives}")
 logging.info(f'Grabbing running plots.')
 jobs, running_work = get_running_plots(jobs=jobs, running_work=running_work,
                                        instrumentation_settings=instrumentation_settings)
+signal(SIGCHLD, SIG_IGN);
 for job in jobs:
     next_job_work[job.name] = datetime.now()
     max_date = None
